d94d1cc5a63fb61f1ad75b14558fa218fc39b630
German <gera.piligrim@gmail.com>
2022-02-15 16:37:47 +0200

    refactoring

--- src/project/main_catalog/utils.py
+++ src/project/main_catalog/utils.py
@@ -2,7 +2,6 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://127.0.0.1:27017/compressors=disabled&gssapiServiceName=mongodb")
 db = client.testdata
-collection = db["collect"]
+collection = db["schema"]
 
 
-#print(list(collection.find({'user': 1})))
