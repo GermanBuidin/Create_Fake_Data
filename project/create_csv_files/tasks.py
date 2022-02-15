4239b8ffc93fbb2d9c37df6b5a0e589983dea6b1
German <gera.piligrim@gmail.com>
2022-02-15 16:31:52 +0200

    refactoring

--- project/create_csv_files/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from .create_file import create_files
-
-
-@shared_task
-def start_create_files(user_id, rows):
-    create_files(user_id, rows)
--- project/main_catalog/utils.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://127.0.0.1:27017/compressors=disabled&gssapiServiceName=mongodb")
-db = client.testdata
-collection = db["schemas"]
-
-
-
